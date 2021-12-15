6dcfa21a60a5605b2f3274dcb8d8a86bb3a95b67
Alexandre Marcelino <amsantos9@gmail.com>
2021-12-15 18:24:14 -0300

    Project created

---
+++ src/main/java/com/example/projetoworkshopsbjava/ProjetoWorkshopSbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoworkshopsbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWorkshopSbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWorkshopSbJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoworkshopsbjava/ProjetoWorkshopSbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoworkshopsbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWorkshopSbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
