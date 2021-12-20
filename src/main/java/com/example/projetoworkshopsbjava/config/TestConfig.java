c44d90259bd809fd9a759c474dfc42fd1173f061
Alexandre Marcelino <amsantos9@gmail.com>
2021-12-20 10:07:10 -0300

    Payment, one-to-one association

--- src/main/java/com/example/projetoworkshopsbjava/config/TestConfig.java
+++ src/main/java/com/example/projetoworkshopsbjava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.projetoworkshopsbjava.entities.Category;
 import com.example.projetoworkshopsbjava.entities.Order;
 import com.example.projetoworkshopsbjava.entities.OrderItem;
+import com.example.projetoworkshopsbjava.entities.Payment;
 import com.example.projetoworkshopsbjava.entities.Product;
 import com.example.projetoworkshopsbjava.entities.User;
 import com.example.projetoworkshopsbjava.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}	
 }
